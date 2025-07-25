5af4e89347d0160a60d3b5283f607c341fd2a985
Muhamad Prasetyo <muhamadprasetyo@example.com>
2025-07-25 22:34:43 +0700

    app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\OptimizePerformance;
-<<<<<<< HEAD
-=======
+
 use App\Http\Middleware\VerifyCsrfToken;
->>>>>>> 6ad12440d924f1a0aa1d26348cd63a38329565ff
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,12 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         // Apply OptimizePerformance middleware to all API routes
         $middleware->api(OptimizePerformance::class);
-<<<<<<< HEAD
-=======
+
+
         
         // Override default VerifyCsrfToken with our custom implementation
         $middleware->replace('csrf', VerifyCsrfToken::class);
->>>>>>> 6ad12440d924f1a0aa1d26348cd63a38329565ff
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
